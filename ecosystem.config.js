ca23690ecfd81cb2c3f3760e48112cd2ac77f2ba
tharindu765 <tharindurandika25@gmail.com>
2026-03-31 03:42:37 +0530

    update pm2 file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,20 @@
 module.exports = {
   apps : [{
-    name: "user-service",
+    name   : "user-service",
     script : "java -jar ./quickhire-user-service/target/user-service-0.0.1-SNAPSHOT.jar",
-    log_file: ".logs/user-service.log",
+    log_file: "./logs/user-service.log",
     instances: 2
   },
-    name: "job-service",
-    script : "java -jar ./quickhire-job-service/target/job-service-0.0.1-SNAPSHOT.jar",
-    log_file: ".logs/job-service.log",
-    instances: 2
+    {
+      name   : "job-service",
+      script : "java -jar ./quickhire-job-service/target/job-service-0.0.1-SNAPSHOT.jar",
+      log_file: "./logs/job-service.log",
+      instances: 2
     },
-     name: "file-service",
-     script : "java -jar ./quickhire-file-service/target/file-service-0.0.1-SNAPSHOT.jar",
-     log_file: ".logs/file-service.log",
-     instances: 2
+      {
+        name   : "file-service",
+        script : "java -jar ./quickhire-file-service/target/file-service-0.0.1-SNAPSHOT.jar",
+        log_file: "./logs/file-service.log",
+        instances: 2
       }]
 }
